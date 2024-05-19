87f8fbc7292e8ef6cce76ed0518202b912916904
sgsure <sgsure1@gmail.com>
2024-05-20 00:08:19 +0300

    first

---
+++ story_content/user.js
@@ -0,0 +1,319 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5hU405zJ8cX":
+        Script1();
+        break;
+      case "5v93LzUsClJ":
+        Script2();
+        break;
+      case "5aVpUQ6sywI":
+        Script3();
+        break;
+      case "5ytm45z1NFa":
+        Script4();
+        break;
+      case "5fp61bcNe3h":
+        Script5();
+        break;
+      case "6V5idPte3rX":
+        Script6();
+        break;
+      case "5gkJCwf5wNp":
+        Script7();
+        break;
+      case "5gLk5ZhAkjY":
+        Script8();
+        break;
+      case "5mGZhGDwWOy":
+        Script9();
+        break;
+      case "64wmN8VCq27":
+        Script10();
+        break;
+      case "5uXRB0le9cD":
+        Script11();
+        break;
+      case "62lld3OSG1V":
+        Script12();
+        break;
+      case "5q33Y70F61F":
+        Script13();
+        break;
+      case "6SvxMfu4r6r":
+        Script14();
+        break;
+      case "5iRBJz0YFYv":
+        Script15();
+        break;
+      case "6dmXNtSzagK":
+        Script16();
+        break;
+      case "5tcx6beJmYp":
+        Script17();
+        break;
+      case "6f5ZX7PUGZd":
+        Script18();
+        break;
+      case "6iK2e8YD4Mv":
+        Script19();
+        break;
+      case "6j00LMo7Abf":
+        Script20();
+        break;
+      case "65B3yk0GwFc":
+        Script21();
+        break;
+      case "6qaQAhvpQpU":
+        Script22();
+        break;
+      case "5kgAoPk6v3a":
+        Script23();
+        break;
+      case "6rKp72Uod33":
+        Script24();
+        break;
+      case "6TrEZoHSjL1":
+        Script25();
+        break;
+      case "6h9ooqhgvFh":
+        Script26();
+        break;
+      case "5bdutw3TXMj":
+        Script27();
+        break;
+      case "5pSHIPkXy3k":
+        Script28();
+        break;
+      case "61ljNlGkjPs":
+        Script29();
+        break;
+      case "6937jeE5UJb":
+        Script30();
+        break;
+      case "6VmcjrPKm5z":
+        Script31();
+        break;
+      case "6g1m1F3gDuN":
+        Script32();
+        break;
+      case "6MULg3mHfs7":
+        Script33();
+        break;
+      case "5XkdVX2espl":
+        Script34();
+        break;
+      case "6BAYdc6DK4J":
+        Script35();
+        break;
+      case "64HGQ9fWQ0E":
+        Script36();
+        break;
+      case "61w4ebzZC3x":
+        Script37();
+        break;
+      case "6aD5cl0Hpi5":
+        Script38();
+        break;
+      case "6NJB0WoKPeI":
+        Script39();
+        break;
+  }
+}
+
+function Script1()
+{
+  window.print()
+}
+
+function Script2()
+{
+  window.print()
+}
+
+function Script3()
+{
+  window.print()
+}
+
+function Script4()
+{
+  window.print()
+}
+
+function Script5()
+{
+  window.print()
+}
+
+function Script6()
+{
+  window.print()
+}
+
+function Script7()
+{
+  window.print()
+}
+
+function Script8()
+{
+  window.print()
+}
+
+function Script9()
+{
+  window.print()
+}
+
+function Script10()
+{
+  window.print()
+}
+
+function Script11()
+{
+  window.print()
+}
+
+function Script12()
+{
+  window.print()
+}
+
+function Script13()
+{
+  window.print()
+}
+
+function Script14()
+{
+  window.print()
+}
+
+function Script15()
+{
+  window.print()
+}
+
+function Script16()
+{
+  window.print()
+}
+
+function Script17()
+{
+  window.print()
+}
+
+function Script18()
+{
+  window.print()
+}
+
+function Script19()
+{
+  window.print()
+}
+
+function Script20()
+{
+  window.print()
+}
+
+function Script21()
+{
+  window.print();
+}
+
+function Script22()
+{
+  window.print();
+}
+
+function Script23()
+{
+  window.print();
+}
+
+function Script24()
+{
+  window.print()
+}
+
+function Script25()
+{
+  window.print()
+}
+
+function Script26()
+{
+  window.print()
+}
+
+function Script27()
+{
+  window.print()
+}
+
+function Script28()
+{
+  window.print()
+}
+
+function Script29()
+{
+  window.print()
+}
+
+function Script30()
+{
+  window.print()
+}
+
+function Script31()
+{
+  window.print()
+}
+
+function Script32()
+{
+  window.print()
+}
+
+function Script33()
+{
+  window.print()
+}
+
+function Script34()
+{
+  window.print()
+}
+
+function Script35()
+{
+  window.print()
+}
+
+function Script36()
+{
+  window.print()
+}
+
+function Script37()
+{
+  window.print()
+}
+
+function Script38()
+{
+  window.print()
+}
+
+function Script39()
+{
+  window.print()
+}
+
